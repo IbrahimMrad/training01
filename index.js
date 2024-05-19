f26de9be7776881b46d9ac5fce14d339bd209f32
admin <ibmrad60@gmail.com>
2024-05-19 21:07:15 +0300

    2nd commit - Calling Students EndPoint

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 function test() {
     alert("Fatima");
 }
+
+function RenderStudents() {
+    ddl_students = document.getElementById('ddl_students');
+    fetch('https://training20240519.onrender.com/students')
+      .then(response => response.json())
+      .then(data => {
+        data.forEach(student => {
+            option = document.createElement('option');
+            option.innerHTML = student.name;
+            ddl_students.appendChild(option);
+        });
+      })
+}
+
+RenderStudents();
